09c19af34b5b621ac77fc46075180fc358d4f51a
danilogazzoli <daniloresende@yahoo.com>
2020-08-10 08:45:08 -0300

    new route to Customers

--- my_api/urls.py
+++ my_api/urls.py
@@ -13,9 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from rest_framework import routers
 from django.contrib import admin
 from django.urls import path
+from customers.api import CustomerViewSet
+
+routes = routers.DefaultRouter()
+routes.register(r'v1/customers', CustomerViewSet, basename="customers")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += routes.urls
+
